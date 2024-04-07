f64943e4fc7b5ad81b6d4cf866376e125b80fe3e
Gorkaaa <gorka.bidaurratzaga@opendeusto.es>
2024-04-07 20:26:09 +0200

    Cambios en el main.c para que se lleven a cabo todos los cambios realizados en las demas clases

--- Proyecto_Deustoteka/main.c
+++ Proyecto_Deustoteka/main.c
@@ -8,54 +8,73 @@
 #include "Menus/menus.h"
 #include "Usuario/listaUsuarios.h"
 #include <stdio.h>
-#define NOMFICH "Usuarios.txt"
+#include <string.h>
+
+#include "bdd.h"
+#include "sqlite3.h"
+#include <stdlib.h>
+
+
+
+
 
 int main(){
-	ListaUsuarios listaU;
+
+
+	sqlite3 *db;
+
+//	int rc = sqlite3_open("Deustoteka.db", &db);
+//
+//	if (rc != SQLITE_OK) {
+//		fprintf(stderr, "Error al abrir la base de datos: %s\n", sqlite3_errmsg(db));
+//		return 1;
+//	}
+//
+//	if (crearBDD(db) != 0) {
+//		fprintf(stderr, "Error al crear la base de datos.\n");
+//		return 1;
+//	}
+
 	Usuario u;
-	int pos;
-	char opcion,opcionP;
+	char opcion, opcionP;
+
 	do{
 		opcion = bienvenidoDeustoteca();
 		switch(opcion){
-			case '1':   u=pedirUsuario();
-						pos = buscarUsuario(listaU, u.nomUsuario);
-						if(pos!=-1){
-							printf("¡Lo sentimos! Este nombre de usuario ya existe\n");
-						}else{
-							registrarUsuario(&u);
-						}
-			break;
-			case '2':   u=pedirUsuario();
-						pos = buscarUsuario(listaU, u.nomUsuario);
-						if(pos==-1){
-							printf("¡Lo sentimos! No tenemos datos de tu registro\n");
-						}else{
-							if(contrasenyaCorrecta(listaU.arrayU[pos].contrasenya, u.contrasenya)){
-								printf("¡Bienvenido!\n"); fflush(stdout);
-								do{
-									opcionP=menuPrincipal();
-									switch(opcionP){
-									case '1':
-									case '2':
-									case '3':printf("Volviendo al menú principal...\n"); fflush(stdout);
-									  break;
-									}
-								}while(opcionP != '3');
-							}else{
-								printf("¡Lo sentimos! La contrasenya no es correcta\n");fflush(stdout);
-							}
+			case '1':	registrarUsuario(&u, db);
 
+			break;
+			case '2':
+				u=pedirUsuario();
+				if(verificarUsuario(u.nomUsuario, u.contrasenya, db)){
+							printf("¡¡Bienvenido!!\n"); fflush(stdout);
+							do{
+								opcionP=menuPrincipal();
+								switch(opcionP){
+								case '1':
+								case '2':
+								case '3':printf("Volviendo al menú principal...\n"); fflush(stdout);
+								break;
+								}
+							}while(opcionP != '3');
+						}else{
+							printf("¡Lo sentimos! La contrasenya no es correcta\n");fflush(stdout);
 						}
+
+
 						break;
 			case '3':
-			case '4': printf("Fin del programa\n");
+			case '4': printf("Fin del programa.\n");
 					  fflush(stdout);
 					  break;
 			default : printf("¡ERROR! La opción no es correcta\n");
 			   	   	  fflush(stdout);
 		}
-	}while(opcion!='4');
+	} while(opcion!='4');
+
+
+	return 0;
+}
 	return 0;
 }
 
